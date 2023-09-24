01508cb82d5c6498b18f4475d534f6fab81c658d
whoisacat <whoisacat@gmail.com>
2023-09-24 20:33:08 +0300

    hw12 readme

---
+++ hw12-hash-tables/src/main/kotlin/ru/otus/algorithms/hash/first/Main.kt
@@ -0,0 +1,6 @@
+package ru.otus.algorithms.hash.first
+
+fun main(args: Array<String>) {
+
+
+}
---
+++ hw12-hash-tables/src/test/kotlin/ru/otus/algorithms/hash/first/Test.kt
@@ -0,0 +1,11 @@
+package ru.otus.algorithms.hash.first
+
+import org.junit.jupiter.api.Test
+
+class Test {
+
+    @Test
+    fun testBubbleSortRandom() {
+
+    }
+}
