8288630b79c3ecce5cfb7e75eb64a5f8c8bbd8d5
whoisacat <whoisacat@gmail.com>
2023-11-23 08:54:28 +0300

    hw21 run length coding todo

---
+++ hw21-run-length-encoding/src/test/kotlin/ru/otus/algorithms/text/runlegthencoding/MainTest.kt
@@ -0,0 +1,10 @@
+package ru.otus.algorithms.text.runlegthencoding
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import ru.otus.algorithms.text.runlegthencoding.*
+
+class MainTest {
+
+}
