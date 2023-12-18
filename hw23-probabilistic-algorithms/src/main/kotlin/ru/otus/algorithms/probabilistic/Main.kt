d218a764431c6884296eb0e2e90e30b66769a63d
whoisacat <whoisacat@gmail.com>
2023-12-18 17:16:45 +0500

    hw23-probabilistic-algorithms init

---
+++ hw23-probabilistic-algorithms/src/main/kotlin/ru/otus/algorithms/probabilistic/Main.kt
@@ -0,0 +1,5 @@
+package ru.otus.algorithms.probabilistic
+
+fun main(args: Array<String>) {
+    println(args)
+}
---
+++ hw23-probabilistic-algorithms/src/test/kotlin/ru/otus/algorithms/probabilistic/MainTest.kt
@@ -0,0 +1,13 @@
+package ru.otus.algorithms.probabilistic
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainTest {
+
+    @Test
+    fun emptyTextTest() {
+        assertTrue(true)
+    }
+}
