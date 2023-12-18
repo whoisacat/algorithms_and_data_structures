f63590bf267beeda2c0f6dcb83fba6cb809bc68d
whoisacat <whoisacat@gmail.com>
2023-12-18 16:54:50 +0500

    hw22 dynamic.programming init

---
+++ hw22-dynamic-programming/src/main/kotlin/ru/otus/algorithms/dynamic/programming/Main.kt
@@ -0,0 +1,5 @@
+package ru.otus.algorithms.dynamic.programming
+
+fun main(args: Array<String>) {
+    println(args)
+}
---
+++ hw22-dynamic-programming/src/test/kotlin/ru/otus/algorithms/dynamic/programming/MainTest.kt
@@ -0,0 +1,13 @@
+package ru.otus.algorithms.dynamic.programming
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainTest {
+
+    @Test
+    fun emptyTextTest() {
+        assertTrue(true)
+    }
+}
