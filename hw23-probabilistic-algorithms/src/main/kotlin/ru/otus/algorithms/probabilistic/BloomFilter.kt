520ea52a2e137b49ba0208ea96e1d643afe80b16
whoisacat <whoisacat@gmail.com>
2024-05-31 20:01:11 +0500

    hw23-probabilistic-algorithms 2 байта - алгоритм изучен https://github.com/whoisacat/algorithms_and_data_structures/tree/hw23-probabilistic-algorithms/hw23-probabilistic-algorithms

---
+++ hw23-probabilistic-algorithms/src/main/kotlin/ru/otus/algorithms/probabilistic/BloomFilter.kt
@@ -0,0 +1,42 @@
+package ru.otus.algorithms.probabilistic
+
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.util.BitSet
+import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.concurrent.read
+import kotlin.concurrent.write
+
+class BloomFilter(private val size: Int) {
+    private val bits = BitSet(size)
+    private val hash = Hash(size)
+    private val lock = ReentrantReadWriteLock()
+
+    fun add(value: ByteArray) {
+        lock.write {
+            for (seed in 1..3) {
+                bits.set(hash.call(seed, value))
+            }
+        }
+    }
+
+    fun mightContains(value: ByteArray): Boolean {
+        lock.read {
+            for (seed in 1..3) {
+                if (!bits.get(hash.call(seed, value))) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private class Hash(val filterSize: Int) {
+        private val md5 = MessageDigest.getInstance("MD5")
+
+        fun call(seed: Int, value: ByteArray) =
+            BigInteger(1, md5.digest(value + seed.toByte()))
+                .remainder(BigInteger.valueOf(filterSize.toLong()))
+                .toInt()
+    }
+}
--- hw23-probabilistic-algorithms/src/main/kotlin/ru/otus/algorithms/probabilistic/Main.kt
+++
@@ -1,5 +0,0 @@
-package ru.otus.algorithms.probabilistic
-
-fun main(args: Array<String>) {
-    println(args)
-}
